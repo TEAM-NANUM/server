0fb7e0ff5ce16665b0763aa6bd007c3ad1a14d3b
hyunjin <guswlsdl0121@gmail.com>
2023-07-24 08:25:34 +0900

    Jenkins Setting

---
+++ src/test/java/server/nanum/NanumApplicationTests.java
@@ -0,0 +1,13 @@
+package server.nanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
