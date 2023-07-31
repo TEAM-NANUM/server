ab6cbcffd753304442e29cb9694002e46d05af34
Seol_JY <wlsdud5654@gmail.com>
2023-08-01 04:43:00 +0900

    :recycle: Missing Annotation 추가

--- src/main/java/server/nanum/repository/SubCategoryRepository.java
+++ src/main/java/server/nanum/repository/SubCategoryRepository.java
@@ -1,10 +1,12 @@
 package server.nanum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import server.nanum.domain.product.SubCategory;
 
 import java.util.List;
 
+@Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
     List<SubCategory> findByCategoryId(Long categoryId);
 }
