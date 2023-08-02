08ea3e0600959120d3962ecb9e4f6b41d4af257c
lhetl <hyun91000@gmail.com>
2023-08-03 00:52:19 +0900

    :recycle: dto 이너클래스 변경
    하나의 리뷰 정보 dto를 전쳬 리뷰 정보 dto 안에 선언하여 사용하게 변경

--- src/main/java/server/nanum/dto/response/MyReviewOrdersDto.java
+++ src/main/java/server/nanum/dto/response/MyReviewOrdersDto.java
@@ -3,19 +3,27 @@ package server.nanum.dto.response;
 import server.nanum.domain.Order;
 import java.util.List;
 
-public record MyReviewOrdersDto(
+public record MyReviewOrdersDTO(
         Integer count,
-        List<MyReviewDto> orders){
-    public static MyReviewOrdersDto toEntity(List<Order> orderList){
-        List<MyReviewDto> DtoList = orderList.stream().map((order)-> {
-            return new MyReviewDto(
+        List<MyReviewDTO> orders){
+    public record MyReviewDTO(
+            Long id,
+            String name,
+            String imgUrl,
+            Float rating,
+            String comment){
+
+    }
+    public static MyReviewOrdersDTO toEntity(List<Order> orderList){
+        List<MyReviewDTO> DtoList = orderList.stream().map((order)-> {
+            return new MyReviewDTO(
                     order.getId(),
                     order.getProduct().getName(),
                     order.getProduct().getImgUrl(),
                     order.getReview().getRating(),
                     order.getReview().getComment());
         }).toList();
-        return new MyReviewOrdersDto(DtoList.size(),DtoList);
+        return new MyReviewOrdersDTO(DtoList.size(),DtoList);
     }
 
 }
--- src/main/java/server/nanum/dto/response/MyUnReviewOrdersDto.java
+++ src/main/java/server/nanum/dto/response/MyUnReviewOrdersDto.java
@@ -3,16 +3,23 @@ package server.nanum.dto.response;
 import server.nanum.domain.Order;
 import java.util.List;
 
-public record MyUnReviewOrdersDto(
+public record MyUnReviewOrdersDTO(
         Integer count,
-        List<MyUnReviewDto> orders){
-    public static MyUnReviewOrdersDto toEntity(List<Order> orderList){
-        List<MyUnReviewDto> DtoList = orderList.stream().map((order)-> {
-            return new MyUnReviewDto(
+        List<MyUnReviewDTO> orders){
+
+    public record MyUnReviewDTO(
+            Long id,
+            String name,
+            String imgUrl){
+    }
+
+    public static MyUnReviewOrdersDTO toEntity(List<Order> orderList){
+        List<MyUnReviewDTO> DtoList = orderList.stream().map((order)-> {
+            return new MyUnReviewDTO(
                     order.getId(),
                     order.getProduct().getName(),
                     order.getProduct().getImgUrl());
         }).toList();
-        return new MyUnReviewOrdersDto(DtoList.size(),DtoList);
+        return new MyUnReviewOrdersDTO(DtoList.size(),DtoList);
     }
 }
