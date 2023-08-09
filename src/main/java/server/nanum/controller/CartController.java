0c308a4ab27d55dba78bfab8faf76ebe00848b18
Seol_JY <wlsdud5654@gmail.com>
2023-08-09 22:22:36 +0900

    :sparkles: 장바구니 기능 관련 권한 지정

--- src/main/java/server/nanum/controller/CartController.java
+++ src/main/java/server/nanum/controller/CartController.java
@@ -3,6 +3,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import server.nanum.annotation.CurrentUser;
 import server.nanum.domain.User;
@@ -18,24 +19,28 @@ public class CartController {
 
     private final CartService cartService;
 
+    @PreAuthorize("hasAnyRole('ROLE_HOST', 'ROLE_GUEST')")
     @GetMapping
     public ResponseEntity<CartResponseDTO.CartList> getCartItems(@CurrentUser User user) {
         CartResponseDTO.CartList cartItems = cartService.getCartItems(user);
         return ResponseEntity.ok(cartItems);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HOST', 'ROLE_GUEST')")
     @PostMapping
     public ResponseEntity<Void> addToCart(@CurrentUser User user,@Valid @RequestBody CartRequestDTO.CartItem cartItem) {
         cartService.addToCart(cartItem, user);
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HOST', 'ROLE_GUEST')")
     @PatchMapping
     public ResponseEntity<CartResponseDTO.CartListItem> updateCartItemQuantity(@CurrentUser User user, @Valid @RequestBody CartRequestDTO.CartItemQuantity cartItemQuantity) {
         CartResponseDTO.CartListItem cartListItem = cartService.updateCartItemQuantity(cartItemQuantity, user);
         return ResponseEntity.ok(cartListItem);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HOST', 'ROLE_GUEST')")
     @PostMapping("/delete")
     public ResponseEntity<Void> removeFromCart(@CurrentUser User user, @RequestBody CartRequestDTO.CartIdList cartIdList) {
         cartService.removeFromCart(cartIdList, user);
