8c5a55b1c79517ae415200951654967f02c7e79e
hyunjin <guswlsdl0121@gmail.com>
2023-08-11 16:53:41 +0900

    update:Submodule 업데이트

--- src/main/java/server/nanum/NanumApplication.java
+++ src/main/java/server/nanum/NanumApplication.java
@@ -1,10 +1,7 @@
 package server.nanum;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class NanumApplication {
