6c6a6a68ba7eadf416fec38f249770805790d9f3
Seol_JY <wlsdud5654@gmail.com>
2023-08-12 15:41:48 +0900

    :spakles: API서버 대응

--- src/main/java/server/nanum/config/SwaggerConfig.java
+++ src/main/java/server/nanum/config/SwaggerConfig.java
@@ -17,8 +17,7 @@ import java.util.List;
                 description = "한채 API 명세서 입니다. 아래 Servers 에서 환경에 맞는 url을 선택하세요.\n ",
                 version = "v1")
             , servers = {
-                    @Server(url = "https://api.hanche.store", description = "Production 환경"),
-                    @Server(url = "http://localhost:8080", description = "Local 환경")
+                    @Server(url = "/", description = "API 서버"),
             }
         )
 @Configuration
