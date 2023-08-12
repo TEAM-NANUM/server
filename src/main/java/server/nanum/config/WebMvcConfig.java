e85603db68698580689071922fb326471fbec66e
Seol_JY <wlsdud5654@gmail.com>
2023-08-13 00:46:29 +0900

    :recycle: 처리되지 않은 예외를 ExceptionResolver에서 받도록 변경

--- src/main/java/server/nanum/config/WebMvcConfig.java
+++ src/main/java/server/nanum/config/WebMvcConfig.java
@@ -1,17 +1,32 @@
 package server.nanum.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import server.nanum.exception.resolver.CustomExceptionResolver;
+import server.nanum.utils.WebhookExceptionHandler;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final WebhookExceptionHandler webhookExceptionHandler;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new CustomExceptionResolver(webhookExceptionHandler));
     }
 }
