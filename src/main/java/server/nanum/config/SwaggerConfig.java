1efab19aec8f340ff40147df1cda31081d2adef8
hyunjin <guswlsdl0121@gmail.com>
2023-08-12 15:45:35 +0900

    gitingore 뺌:

--- src/main/java/server/nanum/config/SwaggerConfig.java
+++ src/main/java/server/nanum/config/SwaggerConfig.java
@@ -16,9 +16,6 @@ import java.util.List;
         info = @Info(title = "한채 API 명세",
                 description = "한채 API 명세서 입니다. 아래 Servers 에서 환경에 맞는 url을 선택하세요.\n ",
                 version = "v1")
-            , servers = {
-                    @Server(url = "https://api.hanche.store")
-            }
         )
 @Configuration
 public class SwaggerConfig {
