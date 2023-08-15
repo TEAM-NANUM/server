ed7fd90e2ae8977184af487712711b61e8e2179a
lhetl <93638355+lhetl@users.noreply.github.com>
2023-08-15 19:56:29 +0900

    :bug: 별점 최솟값 설정
    1점 미만 점수 못주게 변경

--- src/main/java/server/nanum/dto/request/AddReviewDTO.java
+++ src/main/java/server/nanum/dto/request/AddReviewDTO.java
@@ -19,6 +19,7 @@ public record AddReviewDTO( //리뷰 등록 DTO
         @Positive(message = "별점은 양수만 가능합니다")
         @Max(value=5,message = "별점의 최댓값은 5점 입니다")
         @Schema(example = "5.0",description = "상품 별점")
+        @Min(value=1,message = "벌점의 최솟값은 1점 입니다")
         Float rating,
         @NotNull(message = "상품의 후기가 존재하지 않습니다") //후기 작성 없이 별점만 줄 수도 있으니 notnull 사용
         @Schema(example = "음식이 맛있어요",description = "상품 후기")
