dc261a247c45dcc3a00c3da1262f353af157dee3
lhetl <93638355+lhetl@users.noreply.github.com>
2023-08-18 18:03:31 +0900

    :sparkles: 리뷰 별점 낮은순, 최신순 조회(주석처리함)
    일단 만들었습니다

--- src/main/java/server/nanum/repository/ReviewRepository.java
+++ src/main/java/server/nanum/repository/ReviewRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import server.nanum.domain.Review;
+import server.nanum.domain.product.Product;
 
 import java.util.List;
 
@@ -14,5 +16,10 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
     List<Review> findAllByOrderProductId(Long productId);
     @Query(value = "SELECT r FROM Review r ORDER BY r.createAt DESC")
     List<Review> findAllByOrderByCreateAtDesc();
+    @Query(value="SELECT r FROM Review r WHERE r.order.product.id= :productId ORDER BY r.rating ASC")
+    List<Review> findAllByOrderProductIdOrderByRatingAsc(@Param("productId") Long productId);
+
+    @Query(value="SELECT r FROM Review r WHERE r.order.product.id= :productId ORDER BY r.createAt DESC")
+    List<Review> findAllByOrderProductIdOrderByCreateAtDesc(@Param("productId") Long productId);
 
 }
